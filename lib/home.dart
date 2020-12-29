5e70ac15a6c6297919dda1229dc77cb92a4235f4
yassinemehdaoui <yassine08081@gmail.com>
2020-12-29 22:13:24 +0100

    database + authentification OK

--- lib/home.dart
+++ lib/home.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-//import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -10,16 +10,23 @@ class Home extends StatefulWidget {
 
 class HomeState extends State<Home> {
   final DatabaseReference = FirebaseDatabase.instance.reference();
-  //final FirebaseAuth auth = FirebaseAuth.instance;
+  final FirebaseAuth auth = FirebaseAuth.instance;
 
   // Variable for Realtime Database
   final textSending = TextEditingController();
   final textReceiving = TextEditingController();
 
+  // Variable for create account
+  final name_creation = TextEditingController();
+  final email_creation = TextEditingController();
+  final password_creation = TextEditingController();
+
   // Variable for Authentification
-  final name_auth = TextEditingController();
   final email_auth = TextEditingController();
   final password_auth = TextEditingController();
+
+  String status_creation;
+  String status_authentification;
 
   Widget build(BuildContext context) {
     return Scaffold(
@@ -38,10 +45,10 @@ class HomeState extends State<Home> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              /********** Authentification **********/
+              /********** Creation de compte **********/
               Container(
                 child: Text(
-                  'Authentification',
+                  'Création de compte',
                   style: TextStyle(
                     fontSize: 20,
                     fontWeight: FontWeight.w500,
@@ -71,9 +78,80 @@ class HomeState extends State<Home> {
                             border: OutlineInputBorder(),
                             hintText: 'Name',
                           ),
-                          controller: name_auth,
+                          controller: name_creation,
                         ),
                       ),
+                      Container(
+                        //color: Colors.blue[100], // debug
+                        padding: EdgeInsets.all(10),
+                        child: TextField(
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            hintText: 'Email',
+                          ),
+                          controller: email_creation,
+                        ),
+                      ),
+                      Container(
+                        //color: Colors.green[100], // debug
+                        padding: EdgeInsets.all(10),
+                        child: TextField(
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            hintText: 'Password',
+                          ),
+                          controller: password_creation,
+                        ),
+                      ),
+                      Container(
+                        child: Center(
+                          child: Text('status : $status_creation'),
+                        ),
+                      ),
+                      Container(
+                        child: RaisedButton(
+                          onPressed: () {
+                            sign_up();
+                          },
+                          child: Text(
+                            'Sign up',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          color: Colors.blue[900],
+                        ),
+                        alignment: Alignment.center,
+                        //color: Colors.red[100],
+                        padding: EdgeInsets.only(bottom: 10, top: 10),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              /********** Authentification **********/
+              Container(
+                child: Text(
+                  'Authentification',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                alignment: Alignment.centerLeft,
+                padding: EdgeInsets.only(top: 30, bottom: 10),
+              ),
+              Container(
+                //color: Colors.blue[900],
+                //height: 100,
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.blue[900],
+                    width: 3.0,
+                  ),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Container(
+                  child: Column(
+                    children: [
                       Container(
                         //color: Colors.blue[100], // debug
                         padding: EdgeInsets.all(10),
@@ -97,12 +175,17 @@ class HomeState extends State<Home> {
                         ),
                       ),
                       Container(
+                        child: Center(
+                          child: Text('status : $status_authentification'),
+                        ),
+                      ),
+                      Container(
                         child: RaisedButton(
                           onPressed: () {
-                            //sign_up();
+                            sign_in();
                           },
                           child: Text(
-                            'Sign up',
+                            'Sign in',
                             style: TextStyle(color: Colors.white),
                           ),
                           color: Colors.blue[900],
@@ -203,16 +286,40 @@ class HomeState extends State<Home> {
     );
   }
 
-  /*
   void sign_up() {
     try {
       FirebaseAuth.instance.createUserWithEmailAndPassword(
-        email: email_auth.text,
-        password: password_auth.text,
+        email: email_creation.text,
+        password: password_creation.text,
       );
+      print('==> Account created');
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        print('The password provided is too weak.');
+      } else if (e.code == 'email-already-in-use') {
+        print('The account already exists for that email.');
+      }
     } catch (e) {
       print(e);
     }
   }
-  */
+
+  void sign_in() {
+    try {
+      FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: email_auth.text,
+        password: password_auth.text,
+      );
+      status_authentification = 'Account Signed in successfully';
+      print('==> Account Signed in successfully');
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        status_authentification = e.code;
+        print('No user found for that email.');
+      } else if (e.code == 'wrong-password') {
+        status_authentification = e.code;
+        print('Wrong password provided for that user.');
+      }
+    }
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:test_firebase/home.dart';
 import 'package:test_firebase/screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() => runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
