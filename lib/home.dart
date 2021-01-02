6767a34cb66c3d044da48604cc9dd31dcbfcff8c
Yassine MEHDAOUI <yassine08081@gmail.com>
2021-01-02 21:31:40 +0100

    Authentification Firebase

--- lib/home.dart
+++ lib/home.dart
@@ -2,6 +2,7 @@ import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/services.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -17,12 +18,10 @@ class HomeState extends State<Home> {
   final textReceiving = TextEditingController();
 
   // Variable for create account
-  final name_creation = TextEditingController();
   String _email, _password;
 
   // Variable for Authentification
-  final email_auth = TextEditingController();
-  final password_auth = TextEditingController();
+  String _emailAuth, _passwordAuth;
 
   String status_creation, status_authentification;
 
@@ -48,7 +47,7 @@ class HomeState extends State<Home> {
             children: [
               /********** Creation de compte **********/
               Container(
-                color: Colors.cyan[100], // debug
+                //color: Colors.cyan[100], // debug
                 child: Text(
                   'Création de compte',
                   style: TextStyle(
@@ -73,28 +72,47 @@ class HomeState extends State<Home> {
                   key: _formKey,
                   child: Column(
                     children: <Widget>[
-                      TextFormField(
-                        validator: (input) {
-                          if (input.isEmpty) {
-                            return 'Provide an email';
-                          }
-                        },
-                        decoration: InputDecoration(labelText: 'Email'),
-                        onSaved: (input) => _email = input,
+                      Container(
+                        padding: EdgeInsets.all(10),
+                        child: TextFormField(
+                          validator: (input) {
+                            if (input.isEmpty) {
+                              return 'Provide an email';
+                            }
+                          },
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            hintText: 'Email',
+                          ),
+                          onSaved: (input) => _email = input,
+                        ),
                       ),
-                      TextFormField(
-                        validator: (input) {
-                          if (input.length < 6) {
-                            return 'Longer password please';
-                          }
-                        },
-                        decoration: InputDecoration(labelText: 'Password'),
-                        onSaved: (input) => _password = input,
-                        obscureText: true,
+                      Container(
+                        padding: EdgeInsets.all(10),
+                        child: TextFormField(
+                          validator: (input) {
+                            if (input.length < 6) {
+                              return 'Longer password please';
+                            }
+                          },
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            hintText: 'Password',
+                          ),
+                          onSaved: (input) => _password = input,
+                          obscureText: true,
+                        ),
                       ),
-                      RaisedButton(
-                        onPressed: signUp,
-                        child: Text('Sign up'),
+                      Container(
+                        padding: EdgeInsets.all(10),
+                        child: RaisedButton(
+                          onPressed: signUp,
+                          child: Text(
+                            'Sign up',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          color: Colors.blue[900],
+                        ),
                       ),
                     ],
                   ),
@@ -114,7 +132,7 @@ class HomeState extends State<Home> {
               ),
               Container(
                 //color: Colors.blue[900],
-                //height: 100,
+                //height: 30,
                 decoration: BoxDecoration(
                   border: Border.all(
                     color: Colors.blue[900],
@@ -122,49 +140,57 @@ class HomeState extends State<Home> {
                   ),
                   borderRadius: BorderRadius.circular(10),
                 ),
-                child: Container(
+                /*
+                child: Form(
+                  key: _formKey,
                   child: Column(
-                    children: [
+                    children: <Widget>[
                       Container(
-                        //color: Colors.blue[100], // debug
                         padding: EdgeInsets.all(10),
                         child: TextFormField(
+                          validator: (input) {
+                            if (input.isEmpty) {
+                              return 'Provide an email';
+                            }
+                          },
                           decoration: InputDecoration(
                             border: OutlineInputBorder(),
                             hintText: 'Email',
                           ),
-                          controller: email_auth,
+                          onSaved: (input) => _email = input,
                         ),
                       ),
                       Container(
-                        //color: Colors.green[100], // debug
                         padding: EdgeInsets.all(10),
                         child: TextFormField(
+                          validator: (input) {
+                            if (input.length < 6) {
+                              return 'Longer password please';
+                            }
+                          },
                           decoration: InputDecoration(
                             border: OutlineInputBorder(),
                             hintText: 'Password',
                           ),
-                          controller: password_auth,
+                          onSaved: (input) => _password = input,
+                          obscureText: true,
                         ),
                       ),
                       Container(
+                        padding: EdgeInsets.all(10),
                         child: RaisedButton(
-                          onPressed: () {
-                            sign_in();
-                          },
+                          onPressed: signIn,
                           child: Text(
-                            'Sign in',
+                            'Sign up',
                             style: TextStyle(color: Colors.white),
                           ),
                           color: Colors.blue[900],
                         ),
-                        alignment: Alignment.center,
-                        //color: Colors.red[100],
-                        padding: EdgeInsets.only(bottom: 10, top: 10),
                       ),
                     ],
                   ),
                 ),
+                */
               ),
               /********** Réception/Envoi de données **********/
               Container(
@@ -258,7 +284,7 @@ class HomeState extends State<Home> {
     if (_formKey.currentState.validate()) {
       _formKey.currentState.save();
       try {
-        await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        await auth.createUserWithEmailAndPassword(
           email: _email,
           password: _password,
         );
@@ -268,69 +294,17 @@ class HomeState extends State<Home> {
     }
   }
 
-  void sign_in() async {
-    print('sign in button pressed');
-  }
-}
-
-/*
-class SignUpPage extends StatefulWidget {
-  @override
-  _SignUpPageState createState() => _SignUpPageState();
-}
-
-class _SignUpPageState extends State<SignUpPage> {
-  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-  String _email, _password;
-
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(),
-      body: Form(
-        key: _formKey,
-        child: Column(
-          children: <Widget>[
-            TextFormField(
-              validator: (input) {
-                if (input.isEmpty) {
-                  return 'Provide an email';
-                }
-              },
-              decoration: InputDecoration(labelText: 'Email'),
-              onSaved: (input) => _email = input,
-            ),
-            TextFormField(
-              validator: (input) {
-                if (input.length < 6) {
-                  return 'Longer password please';
-                }
-              },
-              decoration: InputDecoration(labelText: 'Password'),
-              onSaved: (input) => _password = input,
-              obscureText: true,
-            ),
-            RaisedButton(
-              onPressed: signUp,
-              child: Text('Sign up'),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
-  void signUp() async {
+  void signIn() async {
     if (_formKey.currentState.validate()) {
       _formKey.currentState.save();
       try {
-        await FirebaseAuth.instance
-            .createUserWithEmailAndPassword(email: _email, password: _password);
-        //Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => LoginPage()));
+        await auth.signInWithEmailAndPassword(
+          email: _email,
+          password: _password,
+        );
       } catch (e) {
         print(e.message);
       }
     }
   }
 }
-*/
